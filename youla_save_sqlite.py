b16db1a31f4904fbb32b6de871b798c252735ed9
lab-05 <lab05@yandex.com>
2020-09-15 22:29:23 +0300

    Delete unused blocks

--- youla_save_sqlite.py
+++ youla_save_sqlite.py
@@ -8,7 +8,6 @@ Created on Fri Jun 19 23:20:24 2020
 import requests
 import json
 from lxml import html
-import os
 import dataset
 
 
@@ -23,31 +22,12 @@ def get_urllist(urls):
     with open(urls) as urlfile:
         urllist = urlfile.read().splitlines()
     return urllist
-
-#user_agent = {'User-agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0'}
     
 def getrequest(url):
     responses = requests.get(url)
     html1 = responses.text
     tree = html.fromstring(html1)
     return html1, tree
-
-def makedir():
-    result = getrequest(url)
-#    n = random.randint(0,300) #посмотри может есть какие идентификаторы объявления
-    start_json_template = "    window.__YOULA_STATE__ = "
-
-    if start_json_template in result[0]:
-        foldername = result[1].xpath('//title/text()')[0].split('–')[0]
-        if os.path.exists(foldername) == False:     
-            foldername = result[1].xpath('//title/text()')[0].split('–')[0]
-            os.mkdir(foldername)        
-        else:
-            foldername = result[1].xpath('//title/text()')[0].split('–')[0]
-#            os.rename(foldername, foldername+str(n))
-            os.mkdir(foldername)
-        os.chdir(foldername)
-
 
 def get_json():
     result_to_json = getrequest(url)
@@ -66,25 +46,16 @@ def save_photo():
     print(photos)
     photos_to_save = ', '.join(photos)
     return photos_to_save
-#    table.insert(dict(Photos=photos_to_save))
 
 def save_description():
     desc_raw = get_json()['entities']['products'][0]['description']
     desc = desc_raw.encode('utf-8').decode('utf-8')
     return desc
-#    table.insert(dict(Description=desc))
 
 def save_name():
     result = getrequest(url)
     name_query = result[1].xpath('//title/text()')[0].split('–')[0]
     return name_query
-#    table.insert(dict(Name=name_query))
-
-
-#def saveinfo():
-#    save_name()
-#    save_description()
-#    save_photo()
 
 def saveinfo():
     table.insert(dict(Name=save_name(), Description=save_description(),Photo=save_photo()))
